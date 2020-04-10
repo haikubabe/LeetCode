21053ebad7811a0f54707a699810a0c3fa30c32a
haikubabe <sreech94@gmail.com>
2020-04-10 16:17:23 +0530

    add min stack

---
+++ thirty_day_challenge/MinStack.java
@@ -0,0 +1,49 @@
+package thirty_day_challenge;
+
+import java.util.Stack;
+
+/**
+ * # Leetcode - #155
+ */
+public class MinStack
+{
+    private Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+        for (int x : stack) {
+            if (x < min) {
+                min = x;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args)
+    {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}

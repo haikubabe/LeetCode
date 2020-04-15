3a62bab74a9e27af9cca2ee7503d43c53383c9c0
haikubabe <sreech94@gmail.com>
2020-04-15 14:53:01 +0530

    add product array

---
+++ thirty_day_challenge/ProductArray.java
@@ -0,0 +1,36 @@
+package thirty_day_challenge;
+
+/**
+ * Leetcode - #238
+ */
+public class ProductArray
+{
+    public static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        int product = 1;
+        for (int i=0;i<nums.length;i++) {
+            if (i != 0)
+            {
+                product = product * nums[i - 1];
+            }
+            output[i] = product;
+        }
+        product = 1;
+        for (int i=nums.length-1;i>=0;i--) {
+            if (i != nums.length-1) {
+                product = product * nums[i+1];
+            }
+            output[i] = output[i] * product;
+        }
+        return output;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums = {1,2,3,4};
+        int[] output = productExceptSelf(nums);
+        for (int n : output) {
+            System.out.print(n + " ");
+        }
+    }
+}
